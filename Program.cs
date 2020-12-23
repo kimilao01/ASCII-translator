770978ad4a8aa707dc227b3dbd7de89e2210f260
kimilao01 <76526337+kimilao01@users.noreply.github.com>
2020-12-24 07:38:06 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ascii_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter somthing: ");
+                string word = Console.ReadLine();
+                foreach (char c in word)
+                {
+                    Console.Write(Convert.ToInt32(c) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
